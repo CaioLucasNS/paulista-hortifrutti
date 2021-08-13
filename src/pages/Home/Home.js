ef33a6c0712b5bfbeb91ffcae3ecdec31b01d4a7
Caio Lucas <caio.lucas@targetwork.com.br>
2021-08-13 00:44:38 -0300

    add routes system and favicon

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./Home.css";
+import Navigation from "../../components/Navigation/Navigation";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <header>{/* <Navigation /> */}</header>
+      <h1>Home aqui</h1>
+      <p>O bagui é doido</p>
+    </div>
+  );
+};
+
+export default Home;
